b42e52772b91ea795145a40df836756d81faedc2
ahmadfauzirahman <ahmadfauzirahman99@gmail.com>
2019-03-07 20:39:50 +0700

    Intergration Sistem Informasi, Pengabdian,Peneletian,Jurnal Dan Buku

---
+++ common/config/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+Yii::setAlias('@common', dirname(__DIR__));
+Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
+Yii::setAlias('@penelitian', dirname(dirname(__DIR__)) . '/penelitian');
+Yii::setAlias('@pengabdian', dirname(dirname(__DIR__)) . '/pengabdian');
+Yii::setAlias('@summary', dirname(dirname(__DIR__)) . '/summary');
+Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
+Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
